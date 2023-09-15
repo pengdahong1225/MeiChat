0eba83a858ef6a354400b76d71d51773a48cfa4e
messi_p <2102533303@qq.com>
2023-09-15 14:40:21 +0800

    sql

--- svr/sql/meiChat_uid.sql
+++ svr/sql/meiChat_uid.sql
@@ -6,7 +6,6 @@ create table if not exists user_info(
     pwd varchar(64) not null,
     gender tinyint default 2,
     pic varchar(256) default '',
-    is_online bool default false,
     
     PRIMARY KEY(uid)
 )engine = InnoDB charset = utf8mb4;
