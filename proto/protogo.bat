f07556efb380aeae4775e5b261c6952d01817e8e
pengdahong1225 <2102533303@qq.com>
2023-09-16 22:19:39 +0800

    bat更新

--- proto/protogo.bat
+++ proto/protogo.bat
@@ -1,6 +1,15 @@
+set connect_proto=../svr/connect/src/proto/
+if not exist "%connect_proto%" (
+   md "%connect_proto%"
+)
+set user_proto=../svr/user/src/proto/
+if not exist "%user_proto%" (
+   md "%user_proto%"
+)
 
-for %%i in (*.proto) do protoc.exe --proto_path=./ --go_out=../svr/connect/src/proto/ %%i
 
-for %%i in (*.proto) do protoc.exe --proto_path=./ --go_out=../svr/user/src/proto/ %%i
+for %%i in (*.proto) do protoc.exe --proto_path=./ --go_out="%connect_proto%" %%i
+
+for %%i in (*.proto) do protoc.exe --proto_path=./ --go_out="%user_proto%" %%i
 
 pause
