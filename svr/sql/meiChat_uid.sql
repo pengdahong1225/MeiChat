1d41fa7b1da0ebc5ededb1109e4fb6642e67653a
pengdahong1225 <2102533303@qq.com>
2023-10-09 10:34:17 +0800

    sql

--- svr/sql/meiChat_uid.sql
+++ svr/sql/meiChat_uid.sql
@@ -3,7 +3,7 @@ use meiChat;
 create table if not exists user_info(
     uid BIGINT NOT NULL,
     pwd VARCHAR(64) NOT null,
-    phone INT not NULL,         # 手机号唯一注册，不能为空
+    phone BIGINT not NULL,         # 手机号唯一注册，不能为空
     email VARCHAR(64) DEFAULT '',
     username VARCHAR(64) DEFAULT '新用户',
     gender tinyint DEFAULT 0,   # 0:woman 1:man
